89997d8d9751855d743ba934d51fe40411057963
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-29 17:30:48 +0530

    1

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -7,10 +7,8 @@ const TaskCard = ({ task }) => {
       <div className="w-1/2 pl-4">
         <h3 className="text-lg font-medium">{task.title}</h3> 
       </div>
-      
       <div className="w-1/2 flex justify-between items-center">
         <span className="text-lg">{task.description}</span>  
-        
         <div className="flex gap-2">
           <CheckCircle2 className="text-green-600" size={20} />
           <Bookmark className="text-yellow-500" size={20} />
